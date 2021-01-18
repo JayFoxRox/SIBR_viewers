e065e177f39d70749426138f70b727ed37dbdc62
unknown <ro.cailleau@gmail.com>
2021-01-18 16:15:50 +0100

    Fixed the GPUQuery. Before, it used to create GL_INVALID_OPERATION (1282) due to a bad indexing of the query.

--- src/core/graphics/GPUQuery.cpp
+++ src/core/graphics/GPUQuery.cpp
@@ -20,9 +20,13 @@ GPUQuery::GPUQuery(GLenum type, size_t count) : _count(count), _type(type){
 	}
 	_ids.resize(count);
 	glGenQueries(GLsizei(_ids.size()), &_ids[0]);
+	_current = _count - 1;
 	// Dummy initial query.
-	begin();
-	end();
+	for (int i = 0; i < _count; ++i)
+	{
+		begin();
+		end();
+	}
 }
 
 void GPUQuery::begin() {
@@ -30,6 +34,7 @@ void GPUQuery::begin() {
 		SIBR_WRG << "Query already started..." << std::endl;
 		return;
 	}
+	_current = (_current + 1) % _count;
 	glBeginQuery(_type, _ids[_current]);
 	_observing = true;
 }
@@ -41,7 +46,6 @@ void GPUQuery::end() {
 	}
 	glEndQuery(_type);
 	_observing = false;
-	_current = (_current + 1) % _count;
 }
 
 uint64 GPUQuery::value() {
@@ -49,10 +53,11 @@ uint64 GPUQuery::value() {
 		SIBR_WRG << "Query still running, ending it first..." << std::endl;
 		end();
 	}
-	// We want the ID of the previous frame, taking into account that we have incremented the counter once more when ending it. So minus 2.
+	// We want the ID of the previous frame, taking into account that we have incremented the counter once more when ending it. So minus 1.
 	// Except if you have only one query, in which case we query this one, but it will stall the GPU.
-	const size_t previous = _count == 1 ? 0 : (_current + _count - 2);
+	const size_t previous = (_current - 1) % _count;
 	GLuint64 data = 0;
 	glGetQueryObjectui64v(_ids[previous], GL_QUERY_RESULT, &data);
+	//CHECK_GL_ERROR;
 	return data;
 }
