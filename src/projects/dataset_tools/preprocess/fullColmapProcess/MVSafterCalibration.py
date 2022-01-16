204483b96d09d137a72728ec0a6f7d7fce3e1eb9
George Drettakis <George.Drettakis@inria.fr>
2022-01-16 22:06:59 +0100

    fixes

--- src/projects/dataset_tools/preprocess/fullColmapProcess/MVSafterCalibration.py
+++ src/projects/dataset_tools/preprocess/fullColmapProcess/MVSafterCalibration.py
@@ -138,7 +138,7 @@ def main():
               for line in source:
                  if line.split():
                     if line.split()[-1] in keys:
-                      print("Writing ", line, " to " , outfile )
+#                      print("Writing ", line, " to " , outfile )
                       outfile.write(line + "\n")
            outfile.close()
            shutil.copyfile(camerasfile, dstpath +"\\cameras.txt")
@@ -146,18 +146,26 @@ def main():
 
     # move all the video images so they are not taken into account in further processing
     imagedir = args["path"] + "\\images"
-    directory = os.listdir(imagedir)
-    searchString = "Video"
+    image_directory = os.listdir(imagedir)
 
-    dstpth = os.path.join(os.path.abspath(os.path.join(args["path"], "images\\")) , "videoImages" )
-    os.makedirs(dstpath, exist_ok=True)
+    videodir = args["path"] + "\\videos"
+    video_directory = os.listdir(videodir)
 
     # move video images
-    for fname in directory: 
-       if searchString in fname:
-         if not os.path.exists(dstpath+fname):
-           shutil.move(os.path.join(imagedir,fname), os.path.join(dstpath,fname))
-           print('moving ', os.path.join(imagedir,fname), " to " , os.path.join(dstpath,fname))
+    cnt = 0
+    for videoname in video_directory: 
+      if os.path.isdir(os.path.join(videodir, videoname)):
+         dstpath = os.path.join(os.path.abspath(os.path.join(args["path"], "images\\")) , videoname)
+         print("Creating ", dstpath)
+         os.makedirs(dstpath, exist_ok=True)
+         for fname in image_directory: 
+           print("Search ", videoname , " in " , fname )
+           if videoname in fname:
+             if not os.path.exists(os.path.join(dstpath,fname)):
+                shutil.move(os.path.join(imagedir,fname), os.path.join(dstpath,fname))
+                print('moving ', os.path.join(imagedir,fname), " to " , os.path.join(dstpath,fname))
+
+    exit()
 
     programs = {
         "colmap": {
