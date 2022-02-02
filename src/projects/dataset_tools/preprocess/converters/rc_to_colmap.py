8cdd60971dc30dd194c801b6a08728c9580e25f2
George Drettakis <George.Drettakis@inria.fr>
2022-02-02 21:43:42 +0100

    fixed rc_to_colmap + verified with ULR

--- src/projects/dataset_tools/preprocess/converters/rc_to_colmap.py
+++ src/projects/dataset_tools/preprocess/converters/rc_to_colmap.py
@@ -73,44 +73,21 @@ def main():
          # to sibr internal
          
          br = np.matrix(cam.rotation).transpose()
-         t = -br * np.matrix([cam.translation[0], cam.translation[1], cam.translation[2]]).transpose()
+         t = -np.matmul(br , np.matrix([cam.translation[0], cam.translation[1], cam.translation[2]]).transpose())
          
          # sibr save to colmap
-         br = br * np.matrix([[1, 0, 0], [0, -1, 0], [0, 0, -1]])
+         br = np.matmul(br, np.matrix([[1, 0, 0], [0, -1, 0], [0, 0, -1]]))
          br = br.transpose()
+
          sci_rot = R.from_matrix(br)
          sci_quat = sci_rot.as_quat()
-         t = -br * t
 
-         outfile.write("{} {} {} {} {} {} {} {} {} {}\n".format(camera_id, sci_quat[3], -sci_quat[0], -sci_quat[1], -sci_quat[2], t[0,0], t[1,0], t[2,0], camera_id, name))
+         t = -np.matmul(br, t)
+
+         outfile.write("{} {} {} {} {} {} {} {} {} {}\n".format(camera_id, -sci_quat[3], -sci_quat[0], -sci_quat[1], -sci_quat[2], t[0,0], t[1,0], t[2,0], camera_id, name))
          camera_id = camera_id + 1
       outfile.close()
 
-    """
-    with open(fname, 'w') as outfile:
-      outfile.write( "# Image list with two lines of data per image:\n" )
-      outfile.write( "#   IMAGE_ID, QW, QX, QY, QZ, TX, TY, TZ, CAMERA_ID, NAME\n" )
-      outfile.write( "#   POINTS2D[] as (X, Y, POINT3D_ID)\n" )
-      for cam in input_bundle.list_of_cameras:
-         name = os.path.basename(input_bundle.list_of_input_images[camera_id-1].path)
-         # colmap internal
-         t = [cam.translation[0], -cam.translation[1], -cam.translation[2]]
-         br = np.matrix(cam.rotation)
-         br [1, 0] = - br[1, 0]
-         br [1, 1] = - br[1, 1]
-         br [1, 2] = - br[1, 2]
-         br [2, 0] = - br[2, 0]
-         br [2, 1] = - br[2, 1]
-         br [2, 2] = - br[2, 2]
-
-         sci_rot = R.from_matrix(br.transpose())
-         sci_quat = sci_rot.as_quat()
-
-         outfile.write("{} {} {} {} {} {} {} {} {} {}\n".format(camera_id, sci_quat[3], sci_quat[0], sci_quat[1], sci_quat[2], t[0], t[1], t[2], camera_id, name))
-         camera_id = camera_id + 1
-      outfile.close()
-      """
-            
 #
 # create points3D.txt
 #
