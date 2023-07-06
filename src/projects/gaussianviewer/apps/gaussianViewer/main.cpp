6bd41975906494776bce66f81af7cc23b6008041
Bernhard Kerbl <kerbl@icg.tugraz.at>
2023-07-06 17:44:40 +0200

    No autofocus for point windows

--- src/projects/gaussianviewer/apps/gaussianViewer/main.cpp
+++ src/projects/gaussianviewer/apps/gaussianViewer/main.cpp
@@ -180,7 +180,7 @@ int main(int ac, char** av)
 	if (myArgs.rendering_mode == 1) 
 		multiViewManager.renderingMode(IRenderingMode::Ptr(new StereoAnaglyphRdrMode()));
 	
-	multiViewManager.addIBRSubView("Point view", gaussianView, usedResolution, ImGuiWindowFlags_ResizeFromAnySide);
+	multiViewManager.addIBRSubView("Point view", gaussianView, usedResolution, ImGuiWindowFlags_ResizeFromAnySide | ImGuiWindowFlags_NoBringToFrontOnFocus);
 	multiViewManager.addCameraForView("Point view", generalCamera);
 
 	// Top view
