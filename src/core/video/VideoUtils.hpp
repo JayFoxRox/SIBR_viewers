6a808a38d79c29609b17936feaff43d5b2b956d8
Bernhard Kerbl <kerbl@icg.tugraz.at>
2023-07-08 14:32:03 +0200

    Not sure why this would be valid

--- src/core/video/VideoUtils.hpp
+++ src/core/video/VideoUtils.hpp
@@ -937,10 +937,10 @@ namespace sibr {
 
 		uint getModeIndice() const {
 			uint mode, mode_size = 0;
-			for (const auto & key_val : bins) {
-				if (key_val.second > mode_size) {
-					mode_size = key_val.second;
-					mode = key_val.first;
+			for (const auto & [key, val] : bins) {
+				if (val > mode_size) {
+					mode_size = val;
+					mode = key;
 				}
 			}
 			return mode;
