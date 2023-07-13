c6d3b76be40e5b072f70146b6ac15a727c1272a4
bkerbl <bkerbl@ad.inria.fr>
2023-07-13 14:49:04 +0200

    Pointer fix

--- src/projects/gaussianviewer/apps/gaussianViewer/main.cpp
+++ src/projects/gaussianviewer/apps/gaussianViewer/main.cpp
@@ -83,7 +83,7 @@ static void User_ReadLine(ImGuiContext*, ImGuiSettingsHandler* handler, void*, c
 			*((bool*)handler->UserData) = true;
 			return;
 		}
-	*((sibr::GaussianView*)handler->UserData)->_dontshow = false;
+	*((bool*)handler->UserData) = false;
 }
 
 static void User_WriteAll(ImGuiContext* imgui_ctx, ImGuiSettingsHandler* handler, ImGuiTextBuffer* buf)
